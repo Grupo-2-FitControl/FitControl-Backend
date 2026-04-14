3e3f3538de81e77537141ccb23f3088741354a00
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-14 21:12:00 +0200

    started proyect and structure maven added

---
+++ src/main/java/com/inditex/FitControl/FitControlApplication.java
@@ -0,0 +1,13 @@
+package com.inditex.FitControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitControlApplication.class, args);
+	}
+
+}
