b4509cd0e8634de25ef69a9c83ee1fe5d92f0788
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-15 21:10:09 +0200

    feat(activity-member): add status code support to BusinessRuleException

--- src/main/java/com/fitcontrol/exception/BusinessRuleException.java
+++ src/main/java/com/fitcontrol/exception/BusinessRuleException.java
@@ -1,7 +1,20 @@
 package com.fitcontrol.exception;
 
 public class BusinessRuleException extends RuntimeException {
+
+    private final int statusCode;
+
     public BusinessRuleException(String message) {
         super(message);
+        this.statusCode = 409;
+    }
+
+    public BusinessRuleException(String message, int statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public int getStatusCode() {
+        return statusCode;
     }
 }
