9a1f3ecf9cb64304686280d4042cbf8bd010e764
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-15 13:27:08 +0200

    feat:teacher model service and repository

--- src/main/java/com/fitcontrol/dto/TeacherDTO.java
+++ src/main/java/com/fitcontrol/dto/TeacherDTO.java
@@ -1,4 +1,19 @@
-package com.fitcontrol.dto;
-
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TeacherDTO {
+    private Long id;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String name;
+
+    @NotBlank(message = "El DNI es obligatorio")
+    @Pattern(regexp = "^[0-9]{8}[A-Z]$", message = "DNI inválido")
+    private String dni;
+
+    @NotNull(message = "El año de contratación es obligatorio")
+    private Integer hiringYear;
+
+    private Boolean isActive;
+    private String imageUrl;
 }
