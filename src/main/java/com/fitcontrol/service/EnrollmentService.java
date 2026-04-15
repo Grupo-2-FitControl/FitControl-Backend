14be2cc0aef8214e412709f9b025864375d8eb9f
ana-morandeira <ana.agrodorolo@gmail.com>
2026-04-15 22:27:25 +0200

    feat(activity-member): implement EnrollmentService interface

--- src/main/java/com/fitcontrol/service/EnrollmentService.java
+++ src/main/java/com/fitcontrol/service/EnrollmentService.java
@@ -1,4 +1,14 @@
 package com.fitcontrol.service;
 
-public class EnrollmentService {
+import com.fitcontrol.dto.ActivityDTO;
+import com.fitcontrol.dto.EnrollmentDTO;
+import com.fitcontrol.dto.MemberDTO;
+
+import java.util.List;
+
+public interface EnrollmentService {
+    EnrollmentDTO enroll(Long activityId, Long memberId);
+    void unenroll(Long activityId, Long memberId);
+    List<ActivityDTO> findActivitiesByMember(Long memberId);
+    List<MemberDTO> findMembersByActivity(Long activityId);
 }
