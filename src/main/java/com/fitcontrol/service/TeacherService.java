c2d4cb13043ab61591e71ef0d8d763b831f029ce
Ana Morandeira <ana.agrodorolo@gmail.com>
2026-04-15 13:28:57 +0200

    TeacherService added

--- src/main/java/com/fitcontrol/service/TeacherService.java
+++ src/main/java/com/fitcontrol/service/TeacherService.java
@@ -1,4 +1,8 @@
-package com.fitcontrol.service;
-
-public class TeacherService {
+public interface TeacherService {
+    List<TeacherDTO> findAll();
+    List<TeacherDTO> findAllActive();
+    TeacherDTO findById(Long id);
+    TeacherDTO create(TeacherDTO dto, MultipartFile image);
+    TeacherDTO update(Long id, TeacherDTO dto, MultipartFile image);
+    void delete(Long id);
 }
